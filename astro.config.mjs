2b8dd3ffb48f5d5efc0ca858d88bb4c90d7bf87d
Jaroslaw Zabiello <hipertracker@gmail.com>
2024-01-12 12:17:47 +0000

    add nanostores devtools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import vue from "@astrojs/vue";
 import { defineConfig } from 'astro/config';
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(
-     {appEntrypoint: '/src/pages/_app'}
-  )]
+  integrations: [
+      vue({appEntrypoint: '/src/pages/_app',  reactivityTransform: true}),
+    sentry(),
+    spotlightjs(),
+    sitemap(),
+  ],
 });
--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,8 +1,23 @@
 import PrimeVue from 'primevue/config';
 import type {App} from 'vue';
-
 import 'primeicons/primeicons.css'
+
+// WARNING: use older @nanostores/vue/devtools == 6.5.1
+// because the new one will not work at the time of
+// this writing (2024-01-12)
+import {devtools} from '@nanostores/vue/devtools'
+
+import { mainStore } from '../stores/main_store';
 
 export default (app: App) => {
     app.use(PrimeVue);
+    app.use(devtools, { mainStore });
 };
+
+if (globalThis.window) {
+    console.log('Hello client-side!');
+}
+if (!globalThis.window) {
+    console.log('Hello server-side!');
+}
+
